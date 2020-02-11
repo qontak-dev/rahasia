f4a3613a8340c640b04622243777b93c3b2cd8ba
burhanudin hakim <udnpico@gmail.com>
2020-02-11 15:50:49 +0700

    Create generator for model

--- lib/rahasia.rb
+++ lib/rahasia.rb
@@ -1,8 +1,15 @@
 # frozen_string_literal: true
 
+require 'active_support'
+# require "active_record"
+require 'pry'
+# ActiveSupport.on_load(:active_record) do
+#   include Hello::Model
+# end
 require 'lockbox'
 require 'vault'
 require 'rahasia/version'
+require 'generators/rahasia/migration_generator'
 
 # Wrapper for encrypt and keep our credentials secret
 module Rahasia
